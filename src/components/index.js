c28a9cdc70b7145e59efabd46793c93ccf29e6f3
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-03-28 16:44:10 +0300

    remove AuthScreen component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import Button from './Button';
 import TextField from './TextField';
-import AuthScreen from './AuthScreen';
 
-export { Button, TextField, AuthScreen };
+export { Button, TextField };
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,32 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AuthScreen, TextField, Button } from '../../components';
 import './Login.css';
 
-const Login = () => (
-    <AuthScreen>
-        <form className="login-form">
-            <div className="username-field">
-                <TextField label="Username" type="text" name="username" />
-            </div>
-            <div className="password-field">
-                <TextField
-                    label="Password"
-                    type="password"
-                    name="password"
-                    helperText={
-                        <Link
-                            className="reset-password-link"
-                            to="/reset-password"
-                        >
-                            Forgot your password?
-                        </Link>
-                    }
-                />
-            </div>
-            <Button>Login</Button>
-        </form>
-    </AuthScreen>
-);
+const Login = () => <div>Login page</div>;
 
 export default Login;
