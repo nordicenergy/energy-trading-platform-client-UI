df7f420f7d001e31f54114ab7c4060fafa057112
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-03-28 10:47:34 +0300

    Login container

--- src/services/routes.js
+++ src/services/routes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { Test, App } from '../containers';
+import { Test, App, Login } from '../containers';
 
 export const Routes = () => (
     <div>
-        <Route path="/" component={App} />
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
         <Route path="/testRoute" component={Test} />
     </div>
 );
