57de6675356de35f5f8cc86dd1857540b1ff70fd
Artur Basak <artur.basak@instinctools.ru>
2018-03-29 17:12:44 +0300

    Merge branch 'master' of https://bitbucket.org/lition/frontend into arturbasak/FE-11
    # Conflicts:
    #	src/components/index.js
    #	src/containers/App/App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { defineMessages } from 'react-intl';
 import { MenuSideBar, Header } from '../../components';
 import './App.css';
 
@@ -9,16 +10,40 @@ class App extends React.Component {
     }
 
     render() {
+        const messages = defineMessages({
+            overview: {
+                id: 'app.menuBar.overview',
+                defaultMessage: 'Overview'
+            },
+            documents: {
+                id: 'app.menuBar.documents',
+                defaultMessage: 'My Documents'
+            },
+            submitMetric: {
+                id: 'app.menuBar.submitMetric',
+                defaultMessage: 'Submit Metric Readings'
+            },
+            trading: {
+                id: 'app.menuBar.trading',
+                defaultMessage: 'Trading'
+            },
+            profile: {
+                id: 'app.menuBar.profile',
+                defaultMessage: 'Profile'
+            }
+        });
+        // const { formatMessage } = this.context.intl;
+
         const items = [
-            { id: '', icon: 'faHome', label: 'Overview' },
-            { id: 'documents', icon: 'faBook', label: 'My Documents' },
+            { id: '', icon: 'faHome', label: messages.documents.defaultMessage },
+            { id: 'documents', icon: 'faBook', label: messages.documents.defaultMessage },
             {
                 id: 'submit_meter',
                 icon: 'faCalculator',
-                label: 'Submit Meter Readings'
+                label: messages.submitMetric.defaultMessage
             },
-            { id: 'trading', icon: 'faChartBar', label: 'Trading' },
-            { id: 'profile', icon: 'faUser', label: 'Profile' }
+            { id: 'trading', icon: 'faChartBar', label: messages.trading.defaultMessage },
+            { id: 'profile', icon: 'faUser', label: messages.profile.defaultMessage }
         ];
 
         return (
