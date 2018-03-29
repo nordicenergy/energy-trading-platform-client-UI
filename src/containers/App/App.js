5dc67f4528af9ea306085c56c2989d022c9fb33e
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-03-29 14:08:59 +0300

    App.js reverted

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavigationCard } from '../../components';
 
 import './App.css';
 
 class App extends React.Component {
     render() {
-        return (
-            <div className="app">
-                <div className="nav-cards-container">
-                    <NavigationCard
-                        type="myProducer"
-                        title="My Producer"
-                        onCardClickHandler={() => {}}
-                    />
-                    <NavigationCard
-                        type="buyEnergy"
-                        title="Buy Energy"
-                        onCardClickHandler={() => {}}
-                    />
-                    <NavigationCard
-                        type="sellEnergy"
-                        title="Sell Energy"
-                        onCardClickHandler={() => {}}
-                    />
-                </div>
-            </div>
-        );
+        return <div className="app" />;
     }
 }
 
