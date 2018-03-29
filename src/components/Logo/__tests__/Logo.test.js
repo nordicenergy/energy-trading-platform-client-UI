017583904e38afac7fc8ca3f335cd12e9ed17faa
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-03-29 09:11:20 +0300

    update Logo component

--- src/components/Logo/__tests__/Logo.test.js
+++ src/components/Logo/__tests__/Logo.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Logo from '../Logo';
 
 function renderComponent(props = {}, mountFn = shallow) {
-    return mountFn(<Logo />);
+    return mountFn(<Logo {...props} />);
 }
 
 describe('<Logo /> component', () => {
@@ -11,8 +11,13 @@ describe('<Logo /> component', () => {
         renderComponent();
     });
 
+    it('should renders with custom class', () => {
+        const component = renderComponent({ className: 'test' });
+        expect(component.hasClass('test')).toBeTruthy();
+    });
+
     it('should render small logo', () => {
         const component = renderComponent({ size: 'small' });
-        component.hasClass('logo-small');
+        expect(component.hasClass('logo-small')).toBeTruthy();
     });
 });
