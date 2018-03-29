fac52f78760989be4e466a24a589be9b32ed5bb7
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-03-29 09:04:29 +0300

    Illustration component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import TextField from './TextField';
 import LoginForm from './LoginForm';
 import Logo from './Logo';
+import Illustration from './Illustration';
 
-export { Button, TextField, LoginForm, Logo };
+export { Button, TextField, LoginForm, Logo, Illustration };
