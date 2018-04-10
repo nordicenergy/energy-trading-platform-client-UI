d4480846b468880a9df4edd7425e5fe288dcaba1
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-04-10 09:24:15 +0300

    ProducerCard component

---
+++ src/components/ProducerCardsPanel/ProducerCard/__tests__/ProducerCard.test.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ProducerCard from '../ProducerCard';
+
+const producerMock = {
+    id: 1,
+    price: 2.9,
+    name: 'John Doe',
+    plantType: 'solar'
+};
+const onClickMock = jest.fn();
+
+function renderComponent(
+    { producer = producerMock, onClick = onClickMock, ...otherProps } = {},
+    mountFn = shallow
+) {
+    return mountFn(
+        <ProducerCard producer={producer} onClick={onClick} {...otherProps} />
+    );
+}
+
+describe('<ProducerCard /> component', function() {
+    it('should renders without errors', () => {
+        renderComponent();
+    });
+
+    it('should renders with correct image', () => {
+        let producerCard = renderComponent();
+        expect(producerCard.html().includes('solarImage')).toBeTruthy();
+
+        producerCard = renderComponent({
+            producer: { ...producerMock, plantType: 'wind' }
+        });
+        expect(producerCard.html().includes('windImage')).toBeTruthy();
+
+        producerCard = renderComponent({
+            producer: { ...producerMock, plantType: 'biomass' }
+        });
+        expect(producerCard.html().includes('biomassImage')).toBeTruthy();
+
+        producerCard = renderComponent({
+            producer: { ...producerMock, plantType: 'other' }
+        });
+        expect(producerCard.html().includes('svg')).toBeFalsy();
+    });
+
+    it('should renders with selected style', () => {
+        const producerCard = renderComponent({ selected: true });
+
+        expect(producerCard.hasClass('producer-card--selected')).toBeTruthy();
+    });
+
+    it('should calls onClick callback', () => {
+        const producerCard = renderComponent();
+
+        producerCard.simulate('click');
+        expect(onClickMock).toHaveBeenCalledWith(producerMock.id);
+
+        onClickMock.mockClear();
+    });
+
+    it('should not calls onClick callback if onClick is not function', () => {
+        const producerCard = renderComponent({ onClick: null });
+
+        producerCard.simulate('click');
+        expect(onClickMock).not.toHaveBeenCalled();
+    });
+});
