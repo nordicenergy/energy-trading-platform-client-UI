6845dd3e828a17993e156d2df83bbd28244b0bc0
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-04-25 16:23:04 +0300

    formatting

--- src/components/DisclosureArrow/DisclosureArrow.js
+++ src/components/DisclosureArrow/DisclosureArrow.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import icons from "@fortawesome/fontawesome-free-solid/index";
+import icons from '@fortawesome/fontawesome-free-solid/index';
 import classNames from 'classnames';
 import './DisclosureArrow.css';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 
 const DisclosureArrow = ({ expanded, onClick }) => (
-    <button className={classNames('disclosure-arrow', { 'expanded': expanded })} onClick={onClick}>
+    <button className={classNames('disclosure-arrow', { expanded: expanded })} onClick={onClick}>
         <FontAwesomeIcon icon={icons['faChevronRight']} />
     </button>
 );
