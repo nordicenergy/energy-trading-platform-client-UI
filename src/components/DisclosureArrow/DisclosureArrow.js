1bade7ec5f63570869bab7662f271c8afdcf6616
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-04-25 16:22:16 +0300

    disclosure component added

---
+++ src/components/DisclosureArrow/DisclosureArrow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import icons from "@fortawesome/fontawesome-free-solid/index";
+import classNames from 'classnames';
+import './DisclosureArrow.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+
+const DisclosureArrow = ({ expanded, onClick }) => (
+    <button className={classNames('disclosure-arrow', { 'expanded': expanded })} onClick={onClick}>
+        <FontAwesomeIcon icon={icons['faChevronRight']} />
+    </button>
+);
+
+DisclosureArrow.propTypes = {
+    expanded: PropTypes.bool,
+    onClick: PropTypes.func
+};
+
+export default DisclosureArrow;
