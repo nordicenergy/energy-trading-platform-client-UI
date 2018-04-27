068a13488d0f604ecf861c2fe170a37e4d4bff79
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-04-27 12:31:14 +0300

    OffersSlider component

---
+++ src/components/OffersSlider/__tests__/OffersSlider.test.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Swiper from 'swiper';
+import OffersSlider from '../OffersSlider';
+
+jest.mock('swiper');
+
+const offersMock = [
+    {
+        id: '01',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'wind',
+        price: 2.9
+    },
+    {
+        id: '02',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'biomass',
+        price: 3.2
+    },
+    {
+        id: '03',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'solar',
+        price: 4
+    },
+    {
+        id: '04',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'wind',
+        price: 2.9
+    },
+    {
+        id: '05',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'biomass',
+        price: 3.2
+    },
+    {
+        id: '06',
+        startPeriod: 1502236800,
+        endPeriod: 1505001600,
+        energyType: 'solar',
+        price: 4
+    }
+];
+function renderComponent({ offers = offersMock, ...otherProps } = {}, mountFn = shallow) {
+    return mountFn(<OffersSlider offers={offers} {...otherProps} />);
+}
+
+describe('<OffersSlider /> component', () => {
+    it('should renders without errors', () => {
+        const offersSlider = renderComponent();
+
+        expect(Swiper).toHaveBeenCalledTimes(1);
+        expect(offersSlider.find('OfferCard')).toHaveLength(6);
+    });
+
+    it('should sets correct refs', () => {
+        const offersSlider = renderComponent({}, mount);
+        const { sliderContainerRef, sliderPrevRef, sliderNextRef, sliderPaginationRef } = offersSlider.instance();
+
+        expect(sliderContainerRef.classList.contains('offers-slider-container')).toBeTruthy();
+        expect(sliderPrevRef.classList.contains('offers-slider-control--prev')).toBeTruthy();
+        expect(sliderNextRef.classList.contains('offers-slider-control--next')).toBeTruthy();
+        expect(sliderPaginationRef.classList.contains('offers-slider-pagination')).toBeTruthy();
+    });
+
+    it('should calls onOfferClick callback', () => {
+        const onOfferClickMock = jest.fn();
+        const offersSlider = renderComponent({ onOfferClick: onOfferClickMock });
+
+        offersSlider
+            .find('OfferCard')
+            .at(1)
+            .props()
+            .onClick();
+        expect(onOfferClickMock).toHaveBeenCalledWith('02');
+    });
+});
