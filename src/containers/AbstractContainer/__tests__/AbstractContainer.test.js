7e544c5c0122fbefaa938a14da98a602a6d2a4ff
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-05-02 11:57:11 +0300

    formatting

--- src/containers/AbstractContainer/__tests__/AbstractContainer.test.js
+++ src/containers/AbstractContainer/__tests__/AbstractContainer.test.js
@@ -3,11 +3,11 @@ import AbstractContainer from '../AbstractContainer';
 import { mount } from 'enzyme';
 
 describe('<AbstractContainer />', () => {
-   it('should call scrollToTop method on componentWillUnmount lifecycle method', () => {
-       AbstractContainer.prototype.scrollToTop = jest.fn();
-       AbstractContainer.prototype.render = jest.fn();
-       const component = mount(<AbstractContainer/>);
-       component.unmount();
-       expect(AbstractContainer.prototype.scrollToTop).toHaveBeenCalled();
-   })
+    it('should call scrollToTop method on componentWillUnmount lifecycle method', () => {
+        AbstractContainer.prototype.scrollToTop = jest.fn();
+        AbstractContainer.prototype.render = jest.fn();
+        const component = mount(<AbstractContainer />);
+        component.unmount();
+        expect(AbstractContainer.prototype.scrollToTop).toHaveBeenCalled();
+    });
 });
