c89dd68d4bc4f6c6a5135ecf3b634e46dd285879
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-05-02 11:27:03 +0300

    test added

---
+++ src/containers/AbstractContainer/__tests__/AbstractContainer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AbstractContainer from '../AbstractContainer';
+import { mount } from 'enzyme';
+
+describe('<AbstractContainer />', () => {
+   it('should call scrollToTop method on componentWillUnmount lifecycle method', () => {
+       AbstractContainer.prototype.scrollToTop = jest.fn();
+       AbstractContainer.prototype.render = jest.fn();
+       const component = mount(<AbstractContainer/>);
+       component.unmount();
+       expect(AbstractContainer.prototype.scrollToTop).toHaveBeenCalled();
+   })
+});
