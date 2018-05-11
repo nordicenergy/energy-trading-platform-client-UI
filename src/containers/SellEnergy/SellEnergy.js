c18abc2b4e4ccc8921c5435ba4dc2cdb504fd66a
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-05-11 19:54:17 +0300

    checking for empty updated data

--- src/containers/SellEnergy/SellEnergy.js
+++ src/containers/SellEnergy/SellEnergy.js
@@ -60,12 +60,12 @@ export class SellEnergy extends AbstractContainer {
 
     componentDidUpdate(prevProps) {
         const { formatMessage } = this.context.intl;
-        const { user, loading, error, ownedProducerOfferInfo } = this.props;
-        if (prevProps.user !== user) {
+        const { user = {}, loading, error, ownedProducerOfferInfo = {} } = this.props;
+        if (user.id && prevProps.user !== user) {
             performGetOwnedProducerOffer(user.id);
         }
 
-        if (prevProps.ownedProducerOfferInfo !== ownedProducerOfferInfo) {
+        if (ownedProducerOfferInfo.id && prevProps.ownedProducerOfferInfo !== ownedProducerOfferInfo) {
             performGetOwnedProducerOffersHistory(ownedProducerOfferInfo.id);
         }
 
