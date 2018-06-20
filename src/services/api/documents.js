54c7dd948f2173d0642c5062e63ed208791cc84d
dzmitry-paluyan <dzmitry.paluyan@instinctools.ru>
2018-06-20 14:41:36 +0300

    BS-162: Integrated with real api for get documents.

--- src/services/api/documents.js
+++ src/services/api/documents.js
@@ -1,50 +1,27 @@
-export function getDocuments(/* page */) {
-    return new Promise(resolve =>
-        setTimeout(resolve, 1000, {
-            data: {
-                numberOfDocuments: 8,
-                documents: [
-                    { id: 1, type: 'invoice', date: 1521911833, name: 'Invoice.pdf', description: 'Annual bill' },
-                    {
-                        id: 2,
-                        type: 'archivedDocument',
-                        date: 1521911833,
-                        name: 'Monthly Installment.pdf',
-                        description: 'Annual bill'
-                    },
-                    { id: 3, type: 'invoice', date: 1521911833, name: 'Annual bill.pdf', description: 'Annual bill' },
-                    {
-                        id: 4,
-                        type: 'invoice',
-                        date: 1521911833,
-                        name: 'Monthly Installment.pdf',
-                        description: 'Annual bill'
-                    },
-                    {
-                        id: 5,
-                        type: 'invoice',
-                        date: 1521911833,
-                        name: 'Monthly Installment.pdf',
-                        description: 'Annual bill'
-                    },
-                    {
-                        id: 6,
-                        type: 'invoice',
-                        date: 1521911833,
-                        name: 'Monthly Installment.pdf',
-                        description: 'Annual bill'
-                    },
-                    {
-                        id: 7,
-                        type: 'archivedDocument',
-                        date: 1521911833,
-                        name: 'Monthly Installment.pdf',
-                        description: 'Annual bill'
-                    },
-                    { id: 8, type: undefined, date: undefined, name: undefined, description: undefined }
-                ]
-            }
-        })
-    );
-    // TODO: return Axios.get(`${SESSION_API_URL}/documents/${userId}`);
+import Axios from 'axios';
+import { SESSION_API_URL, LIMIT } from '../../constants';
+
+export function getDocuments(page = 0) {
+    // TODO: Remove contractId & invoiceNumber from params and do correct url
+    const generateUrl = (document = {}) => {
+        if (!['invoice', 'archivedDocument'].includes(document.type)) {
+            return null;
+        }
+        let url = `/v0.1/documents/download?contractId=700`;
+        if (document.type === 'invoice') {
+            url += `&invoiceNumber=${document.id}`;
+        }
+        if (document.type === 'archivedDocument') {
+            url += `&archivedDocumentID=${document.id}`;
+        }
+        return url;
+    };
+
+    return Axios.get(`${SESSION_API_URL}/documents`, {
+        params: { limit: LIMIT, offset: page * LIMIT, contractId: 700 }
+    }).then(response => {
+        const { documents = [] } = response.data;
+        response.data.documents = documents.map(d => ({ ...d, url: generateUrl(d) }));
+        return response;
+    });
 }
