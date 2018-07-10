198cfaecd3dfc17f44b2078897608fc295107184
dzmitry-paluyan <dzmitry.paluyan@instinctools.ru>
2018-07-10 12:12:51 +0300

    BS-208: Fixed remarks from Vladnick.

--- src/components/MeterReadingsHistory/MeterReadingsHistory.js
+++ src/components/MeterReadingsHistory/MeterReadingsHistory.js
@@ -5,29 +5,25 @@ import { MONTH_DAY_DATE_FORMAT } from '../../constants';
 
 import './MeterReadingsHistory.css';
 
-class MeterReadingsHistory extends React.PureComponent {
-    render() {
-        const { title, data, consumptionUnitLabel } = this.props;
-
-        return (
-            <table className="meter-readings-history">
-                <caption>{title}</caption>
-                <tbody>
-                    {data.map((item, index) => {
-                        return (
-                            <tr key={`${item.date}${index}`}>
-                                <td>{item.date ? formatDate(item.date, MONTH_DAY_DATE_FORMAT) : '-'} </td>
-                                <td>
-                                    {Number.isFinite(item.value) ? item.value : '-'} {consumptionUnitLabel || '-'}
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        );
-    }
-}
+const MeterReadingsHistory = ({ title, data, consumptionUnitLabel }) => {
+    return (
+        <table className="meter-readings-history">
+            <caption>{title}</caption>
+            <tbody>
+            {data.map((item, index) => {
+                return (
+                    <tr key={`${item.date}${index}`}>
+                        <td>{item.date ? formatDate(item.date, MONTH_DAY_DATE_FORMAT) : '-'} </td>
+                        <td>
+                            {Number.isFinite(item.value) ? item.value : '-'} {consumptionUnitLabel || '-'}
+                        </td>
+                    </tr>
+                );
+            })}
+            </tbody>
+        </table>
+    );
+};
 
 MeterReadingsHistory.propTypes = {
     data: PropTypes.arrayOf(PropTypes.object),
