48192c2f98e205952e328c05f3e77757b0730413
Nick333 <uladzislau.nikulcha@instinctools.ru>
2018-08-10 17:30:40 +0300

    breadcrumbs are translated when locale is changed

--- src/containers/Producer/Producer.js
+++ src/containers/Producer/Producer.js
@@ -25,7 +25,9 @@ export class Producer extends AbstractContainer {
             producer: state.Producers.producer.data,
             profile: state.Users.profile.data,
             selectedProducer: state.Producers.selectedProducer.data,
-            error: state.Producers.producer.error || state.Users.profile.error || state.Producers.selectedProducer.error
+            error:
+                state.Producers.producer.error || state.Users.profile.error || state.Producers.selectedProducer.error,
+            locale: state.App.localization.data.locale
         };
     }
 
@@ -38,9 +40,9 @@ export class Producer extends AbstractContainer {
 
     componentDidUpdate(prevProps) {
         const { producer: prevProducer = {}, selectedProducer: prevSelectedProducer = {}, error: oldError } = prevProps;
-        const { producer = {}, selectedProducer = {}, error, loading } = this.props;
+        const { producer = {}, selectedProducer = {}, error, loading, locale } = this.props;
 
-        if (prevProducer.id !== producer.id || prevProducer.name !== producer.name) {
+        if (prevProducer.id !== producer.id || prevProducer.name !== producer.name || locale !== prevProps.locale) {
             this.setupProducerBreadcrumbs();
         }
 
@@ -144,7 +146,8 @@ Producer.propTypes = {
     loading: PropTypes.bool,
     producer: PropTypes.object,
     profile: PropTypes.object,
-    error: PropTypes.object
+    error: PropTypes.object,
+    locale: PropTypes.string
 };
 
 Producer.defaultProps = {
