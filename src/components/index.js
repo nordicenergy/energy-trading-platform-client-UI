6fdb5e804cd1f8907ede0abde0d987d1f60d2e3b
Aliaksandr Zimakou <aliaksandr.zimakou@instinctools.ru>
2018-10-16 14:50:03 +0300

    <Alert /> component

--- src/components/index.js
+++ src/components/index.js
@@ -45,3 +45,4 @@ export { default as HelpIcon } from './HelpIcon';
 export { default as Wizard } from './Wizard';
 export { default as RegistrationForm } from './RegistrationForm';
 export { default as Captcha } from './Captcha';
+export { default as Alert } from './Alert';
