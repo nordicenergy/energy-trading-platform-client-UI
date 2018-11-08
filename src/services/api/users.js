f22ca60e10389549c208a6282fd0d1bf003b2729
Artur Basak <archik@archikmac.local>
2018-11-08 16:52:33 +0300

    temp fhot fix for contract data

--- src/services/api/users.js
+++ src/services/api/users.js
@@ -25,6 +25,7 @@ export function getUserData() {
         return {
             data: {
                 user: {
+                    ...user.contract, // TODO @aliaksandr.stashkevich: please remove this workaround  after contract data tab completion
                     ...user,
                     birthday: moment(formattedBirthdayData).unix() // convert to unix time stamp
                 }
