33c5e99b59f4afb4fc1da4b7ede160826e57fb23
Aliaksandr Stashkevich <aliaksandr.stashkevich@instinctools.ru>
2018-11-12 16:42:42 +0300

    BS-368: Remove email contract field from profile messages

--- src/services/translations/messages/Profile.js
+++ src/services/translations/messages/Profile.js
@@ -109,10 +109,6 @@ const messages = defineMessages({
         id: 'app.profilePage.labels.contract.birthday',
         defaultMessage: 'Date of birth'
     },
-    contractEmail: {
-        id: 'app.profilePage.labels.contract.email',
-        defaultMessage: 'Email'
-    },
     paymentMethod: {
         id: 'app.profilePage.labels.paymentMethod',
         defaultMessage: 'Payment options'
